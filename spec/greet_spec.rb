acd51dcfa5cecbcc0ec687c9bf58726282d90552
sophie <soph_lew@hotmail.co.uk>
2022-07-19 23:50:18 +0100

    Challenges and tests completed

--- spec/greet_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'greet'
-RSpec.describe "greet method" do
-  it "returns a greeting message" do
-    result = greet("Sophie")
-    expect(result).to eq "Hello, Sophie!"
-  end
-end
--- spec/report_length_spec.rb
+++
@@ -1,7 +0,0 @@
-require "report_length"
-RSpec.describe "report_length method" do
-  it "reports back the length of the string" do
-    result = report_length("hello")
-    expect(result).to eq "This string was 5 characters long."
-  end
-end
